5e7cad112fae06a5ec1afcce8543c5a57a6adfc9
ben wiseley <wiseleyb@Mac-san.local>
2009-03-17 16:03:47 -0700

    bug fix

--- app/models/schedule_detail.rb
+++ app/models/schedule_detail.rb
@@ -3,8 +3,11 @@ class ScheduleDetail < ActiveRecord::Base
   
   def self.find_or_create(sid, d)
     #TODO - make this DB independent
-    sd = ScheduleDetail.find(:first, 
-        :conditions => ["schedule_id = ? and date(schedule_date) = ?", sid, d.strftime("%Y-%m-%d")])
+    sd = nil
+    if !sid.blank? && !d.blank?
+      sd = ScheduleDetail.find(:first, 
+          :conditions => ["schedule_id = ? and date(schedule_date) = ?", sid, d.strftime("%Y-%m-%d")])
+    end
     if sd.nil?
       sd = ScheduleDetail.new(:schedule_id => sid, :schedule_date => d)
     end
